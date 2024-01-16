400a578576cc045cb4648879d981bef2d7b4f562
orenatogouvea <contatorenatogouvea@gmail.com>
2024-01-16 12:02:00 -0300

    projeto goflix

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>GOFLIX &copy; Desenvolvido por Renato Gouvêa Jr 2023</h2>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/SearchVideoList/index.js
@@ -0,0 +1,30 @@
+import styles from "./SearchVideoList.module.css";
+import VideoList from "../../components/VideoList";
+import { useState } from "react";
+
+function filterVideos(videos, searchText){
+    return videos.filter((video) => video.category.includes(searchText) || video.title.includes(searchText))
+}
+
+function SearchVideoList({ videos }) {
+
+    const [ searchText, setSearchText ] = useState('')
+    const foundVideos = filterVideos(videos, searchText)
+
+    return (
+        <section className={styles.container}>
+            <input 
+                type="search"
+                placeholder="Pesquisar..."
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)}
+            />
+            <VideoList 
+                videos={foundVideos} 
+                emptyHeading={`Sem vídeos sobre "${searchText}"`}
+            />
+        </section>
+    );
+}
+
+export default SearchVideoList;
